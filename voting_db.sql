0f35eb4dc0be420ee473f8a06bea421c891ff6a6
Pranav shelke <88943865+PranavShelke4@users.noreply.github.com>
2022-05-23 11:49:31 +0530

    Add files via upload

---
+++ voting_db.sql
@@ -0,0 +1,227 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: May 23, 2022 at 08:17 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `voting_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category_list`
+--
+
+CREATE TABLE `category_list` (
+  `id` int(30) NOT NULL,
+  `category` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `category_list`
+--
+
+INSERT INTO `category_list` (`id`, `category`) VALUES
+(1, 'President'),
+(3, 'Vice Presindent');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(30) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 2 COMMENT '1+admin , 2 = users'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `username`, `password`, `type`) VALUES
+(1, 'Administrator', 'admin', 'admin123', 1),
+(2, 'Voter 2', 'voter2', 'voter2', 2),
+(3, 'Voter 1', 'voter1', 'voter1', 2),
+(4, 'Voter 3', 'voter3', 'voter3', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `votes`
+--
+
+CREATE TABLE `votes` (
+  `id` int(30) NOT NULL,
+  `voting_id` int(30) NOT NULL,
+  `category_id` int(30) NOT NULL,
+  `voting_opt_id` int(30) NOT NULL,
+  `user_id` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `votes`
+--
+
+INSERT INTO `votes` (`id`, `voting_id`, `category_id`, `voting_opt_id`, `user_id`) VALUES
+(16, 1, 1, 16, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `voting_cat_settings`
+--
+
+CREATE TABLE `voting_cat_settings` (
+  `id` int(30) NOT NULL,
+  `voting_id` int(30) NOT NULL,
+  `category_id` int(30) NOT NULL,
+  `max_selection` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `voting_list`
+--
+
+CREATE TABLE `voting_list` (
+  `id` int(30) NOT NULL,
+  `title` varchar(250) NOT NULL,
+  `description` text NOT NULL,
+  `is_default` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `voting_list`
+--
+
+INSERT INTO `voting_list` (`id`, `title`, `description`, `is_default`) VALUES
+(1, 'President Voting', 'Only For President Voting', 1),
+(3, 'Vice President Voting', 'Only For Vice President Voting', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `voting_opt`
+--
+
+CREATE TABLE `voting_opt` (
+  `id` int(30) NOT NULL,
+  `voting_id` int(30) NOT NULL,
+  `category_id` int(30) NOT NULL,
+  `image_path` text NOT NULL,
+  `opt_txt` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `voting_opt`
+--
+
+INSERT INTO `voting_opt` (`id`, `voting_id`, `category_id`, `image_path`, `opt_txt`) VALUES
+(15, 1, 1, '1653276600_1600418640_avatar.jpg', 'Pranav Shelke'),
+(16, 1, 1, '1653276600_1600418640_avatar.jpg', 'Avadhut Shedage');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `category_list`
+--
+ALTER TABLE `category_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `votes`
+--
+ALTER TABLE `votes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `voting_cat_settings`
+--
+ALTER TABLE `voting_cat_settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `voting_list`
+--
+ALTER TABLE `voting_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `voting_opt`
+--
+ALTER TABLE `voting_opt`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `category_list`
+--
+ALTER TABLE `category_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `votes`
+--
+ALTER TABLE `votes`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `voting_cat_settings`
+--
+ALTER TABLE `voting_cat_settings`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `voting_list`
+--
+ALTER TABLE `voting_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `voting_opt`
+--
+ALTER TABLE `voting_opt`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
